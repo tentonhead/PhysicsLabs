e8a4fad6f74a053154091807a3e6b6fe7a6f6a35
tentonhead <alextolstykh41@gmail.com>
2024-04-22 23:58:51 +0300

    added graph png to repo

--- 2Sem/5/graph.py
+++ 2Sem/5/graph.py
@@ -72,6 +72,7 @@ if __name__ == "__main__":
     plt.scatter(t, one_by_ε, color="black", s=10)
     #plt.plot(t, one_by_ε, color="black")
     plt.show()
+    plt.savefig("graph.png")
 
     output_file = open("output.csv", "w")
     output_file.write("| t, ℃  | C, нФ |  ε, 1/мм  | 1/ε, мм  |\n")
